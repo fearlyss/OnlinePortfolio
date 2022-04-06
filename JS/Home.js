911020a890b3f61c9e9f906da531126d42ad2cd3
Alyssa Dames <alyssa.dames@gmail.com>
2022-04-05 22:57:22 -0400

    NewHome

---
+++ JS/Home.js
@@ -0,0 +1,20 @@
+let triFunction = document.getElementById("triangle");
+triFunction.addEventListener("mouseenter", function ( event ) {
+    event.target.style.border = 'border-right: 50px solid red';
+
+    setTimeout(function() {
+        event.target.style.color = "";
+      }, 500);
+    }, false);
+    
+    // This handler will be executed every time the cursor
+    // is moved over a different list item
+    triFunction.addEventListener("mouseover", function( event ) {
+      // highlight the mouseover target
+      event.target.style.color = "orange";
+    
+      // reset the color after a short delay
+      setTimeout(function() {
+        event.target.style.color = "";
+      }, 500);
+    }, false);
